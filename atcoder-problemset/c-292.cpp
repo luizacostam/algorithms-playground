19c9eb9881ea8035c95f1d4642e8f49ea68c6309
luizacostam <luizacm0209@gmail.com>
2023-03-08 17:00:24 -0300

    adding new problem

--- atcoder-problemset/c-292.cpp
+++ atcoder-problemset/c-292.cpp
@@ -2,9 +2,17 @@
 using namespace std;
 
 int main(){
-    int n;
+    int n, sum = 0;
     cin >> n;
+    int array[n+1];
+    memset(array, 0, sizeof(array));
     for(int a = 1; a <= n; a++){
-        for(int b = )
+        for(int b = 1; a*b <= n; b++){
+            array[a*b]++;
+        }
     }
+    for(int a = 1; a <= n; a++){
+        sum += array[a] * array[n-a];
+    }
+    cout << sum << endl;
 }
